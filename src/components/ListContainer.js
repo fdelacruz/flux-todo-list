dc8dbfd36fd6357b8cc0431a88430e4912c9bb78
Francisco De La Cruz <helloquico@gmail.com>
2015-10-15 18:22:49 -0400

    Initial commit

---
+++ src/components/ListContainer.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var ListContainer = React.createClass({
+
+	render: function () {
+		return (
+				<h1>Great Scott, it works!</h1>
+		);
+	}
+});
+
+module.exports = ListContainer;
